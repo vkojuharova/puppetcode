716bc5a4080ade2566a0b29acf050baf5e06475e
detraining <detraining@puppetlabs.vm>
2013-04-10 13:24:51 +0000

    Add apache for lab

---
+++ modules/apache/manifests/init.pp
@@ -0,0 +1,25 @@
+class apache {
+  package { 'httpd':
+    ensure => present,
+  }
+  file { '/etc/https/conf/httpd.conf':
+    ensure    => file ,
+      owner   => 'root',
+      group   => 'root',
+      require => Package['httpd'],
+  }
+  file { '/var/www':
+    ensure => directory,
+  }
+  file { '/var/www/html':
+    ensure => directory,
+  }
+  file { '/var/www/html/index.html':
+    ensure => file ,
+    source => 'puppet:///modules/apache/index.html',
+  }
+  service { 'httpd':
+    ensure    => running,
+    susbscibe => File ['/etc/httpd/conf/httpd.conf'] ,
+  }
+}
