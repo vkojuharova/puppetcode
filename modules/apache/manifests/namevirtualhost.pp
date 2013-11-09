c10ddccbe5df3038b067d8e6cd08d90b5ac0535d
Vanja Kojuharova <vanja_kojuharova@harvard.edu>
2013-11-09 00:17:50 -0500

    combining files, instead of using concat

--- modules/apache/manifests/namevirtualhost.pp
+++ modules/apache/manifests/namevirtualhost.pp
@@ -2,9 +2,17 @@ define apache::namevirtualhost {
   $addr_port = $name
   include apache::params
 
+#  file { $apache::params::ports_file:
+#           ensure => file ,
+#           content => template('apache/listen.erb'),
+#           notify  => Class['Apache::Service'],
+#           require => Package['httpd'],
+#         }
+
+# Do not use that . Imported to listen.erb
   # Template uses: $addr_port
-  concat::fragment { "NameVirtualHost ${addr_port}":
-    target  => $apache::params::ports_file,
-    content => template('apache/namevirtualhost.erb'),
-  }
+#  concat::fragment { "NameVirtualHost ${addr_port}":
+#    target  => $apache::params::ports_file,
+#    content => template('apache/namevirtualhost.erb'),
+#  }
 }
