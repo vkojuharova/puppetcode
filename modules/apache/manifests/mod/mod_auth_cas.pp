206eae3a91340eb68796b993c3faa4b9b08304c3
Vanja Kojuharova <vanja_kojuharova@harvard.edu>
2013-11-11 21:56:46 -0500

    Test adding mod_auth_cas

--- modules/apache/manifests/mod/mod_auth_cas.pp
+++ modules/apache/manifests/mod/mod_auth_cas.pp
@@ -25,7 +25,7 @@ class apache::mod::mod_auth_cas {
     }
     file {'mod_auth_cas.so':
         ensure  => file,
-        path    => "${apache::params::lib_path}/mod_auth_cas.so",
+        path    => "/etc/httpd/modules/mod_auth_cas.so",
         source  => 'puppet:///modules/apache/modules/mod_ath_cas.so',
         group   => 'root',
         owner   => 'root',
