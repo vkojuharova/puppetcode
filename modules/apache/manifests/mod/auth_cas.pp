d419ae43b1b04183add1db7b9eb61ccb59754799
Vanja Kojuharova <vanja_kojuharova@harvard.edu>
2013-11-12 17:48:11 -0500

    Proxy_dest

--- modules/apache/manifests/mod/auth_cas.pp
+++ modules/apache/manifests/mod/auth_cas.pp
@@ -46,8 +46,6 @@ class apache::mod::auth_cas {
         docroot => '/var/www/castest',
         ssl     => true,
         mod_auth_cas => true,
-#        proxy_pass => true,
-        proxy_dest => /castest ajp://$name:8003/castest #,
-#        proxy_ajp  => true,
+        proxy_dest => "/castest ajp://$name:8003/castest"
     }
 }
