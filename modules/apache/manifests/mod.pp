6cdf9b85c85e9046e222bfcbf312c577107b239a
Vanja Kojuharova <vanja_kojuharova@harvard.edu>
2013-11-13 09:43:22 -0500

    Proxy_dest

--- modules/apache/manifests/mod.pp
+++ modules/apache/manifests/mod.pp
@@ -15,10 +15,6 @@ define apache::mod (
   # Determine if we have special lib
   $mod_libs = $apache::params::mod_libs
 
-  notice("mod_lib $mod_libs and lib is $lib")
-
-  notice("mod_lib $mod_libs[$mod]")
-
   $mod_lib = $mod_libs[$mod] # 2.6 compatibility hack
   if $lib {
     $lib_REAL = $lib
@@ -27,8 +23,6 @@ define apache::mod (
   } else {
     $lib_REAL = "mod_${mod}.so"
   }
-
-notice ("lib_REAL is $lib_REAL")
 
   # Determine if we have a package
   $mod_packages = $apache::params::mod_packages
